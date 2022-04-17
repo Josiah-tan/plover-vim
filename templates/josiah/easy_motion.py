841eb3ec312bef912ed0bd18292ada232d7f6ea7
josiah tan <josiah123t@gmail.com>
2022-04-17 14:22:03 +1000

    added vimium easy motion

--- templates/josiah/easy_motion.py
+++ templates/josiah/easy_motion.py
@@ -13,7 +13,6 @@ from josiah_modifier.defaults import spelling
 
 LONGEST_KEY = 2
 
-
 easy_motion_lookup = EasyMotionLookup({
     "disable_defaults": False,
     # any dictionary entry overiddes the defaults
@@ -29,8 +28,35 @@ easy_motion_lookup = EasyMotionLookup({
             # some ideas for others if you ever run out: -TZ, -SD, -TDZ, -SDZ, -TSZ, -TSD, or -TSDZ
             "mods": {
                 # "-FPB": "",  #
-                # "-FP": "",  #
+                # "-FP": "",  # CHrome
                 "-FB": "space space f",  # Forward Back
+                # "-PB": "shift(t)",  # Previous Backwards (in command_letter)
+                # "-F": "f",  # Forwards (in command_letter)
+                # "-P": "t",  # Previous (in command_letter)
+                # "-B": "shift(f)",  # Backwards (in command_letter)
+                # "": ""  # for normal commands (in command_letter)
+                }
+            },
+        ]})
+
+
+chrome_lookup = EasyMotionLookup({
+    "disable_defaults": False,
+    # any dictionary entry overiddes the defaults
+    "spelling": {"TP": "f"},  # dict: right handed only finger spelling
+    "symbols": {},  # dict: left hand symbols
+    "shifted": {},  # set: any symbols that should be shifted
+    "escape": "escape",  # default: "escape"
+    "right_hand": {},  # dict: easymotion finger spelling (right hand)
+    "left_hand": {},  # dict: easymotion finger spelling (left hand)
+    "systems": [
+        {
+            "unique_ender": "LTDZ",
+            # some ideas for others if you ever run out: -TZ, -SD, -TDZ, -SDZ, -TSZ, -TSD, or -TSDZ
+            "mods": {
+                # "-FPB": "",  #
+                "-FP": "",  # CHrome (vimium)
+                # "-FB": "space space f",  # Forward Back
                 # "-PB": "shift(t)",  # Previous Backwards (in command_letter)
                 # "-F": "f",  # Forwards (in command_letter)
                 # "-P": "t",  # Previous (in command_letter)
@@ -45,10 +71,19 @@ assert easy_motion_lookup.dictionary.longest_key == LONGEST_KEY
 
 
 def lookup(key):
-    for look in [easy_motion_lookup]:
+    for look in [
+            easy_motion_lookup,
+            chrome_lookup,
+            ]:
         with suppress(KeyError):
             return look(key)
 
 
 if __name__ == "__main__":
-    easy_motion_lookup.generateJson()
+    # easy_motion_lookup.generateJson()
+    # chrome_lookup.generateJson()
+    import json
+    dict = {**chrome_lookup.dictionary.items_str_dict(),
+            }
+    with open("templates/josiah/easy_motion.json", "w") as f:
+        json.dump(dict, f, ensure_ascii=False, indent=0)
