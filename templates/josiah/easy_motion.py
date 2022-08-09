4dfa096a4d5b718f073948a846e00c7bfefc2698
Josiah-tan <josiah123t@gmail.com>
2022-08-09 22:54:13 +1000

    added back in easy motion for chrome
    just realized that I needed to use the double sided fingerspelling

--- templates/josiah/easy_motion.py
+++ templates/josiah/easy_motion.py
@@ -40,31 +40,31 @@ easy_motion_lookup = EasyMotionLookup({
         ]})
 
 
-# chrome_lookup = EasyMotionLookup({
-#     "disable_defaults": False,
-#     # any dictionary entry overiddes the defaults
-#     "spelling": {"TP": "f"},  # dict: right handed only finger spelling
-#     "symbols": {},  # dict: left hand symbols
-#     "shifted": {},  # set: any symbols that should be shifted
-#     "escape": "escape",  # default: "escape"
-#     "right_hand": {},  # dict: easymotion finger spelling (right hand)
-#     "left_hand": {},  # dict: easymotion finger spelling (left hand)
-#     "systems": [
-#         {
-#             "unique_ender": "LTDZ",
-#             # some ideas for others if you ever run out: -TZ, -SD, -TDZ, -SDZ, -TSZ, -TSD, or -TSDZ
-#             "mods": {
-#                 # "-FPB": "",  #
-#                 "-FP": "",  # CHrome (vimium)
-#                 # "-FB": "space space f",  # Forward Back
-#                 # "-PB": "shift(t)",  # Previous Backwards (in command_letter)
-#                 # "-F": "f",  # Forwards (in command_letter)
-#                 # "-P": "t",  # Previous (in command_letter)
-#                 # "-B": "shift(f)",  # Backwards (in command_letter)
-#                 # "": ""  # for normal commands (in command_letter)
-#                 }
-#             },
-#         ]})
+chrome_lookup = EasyMotionLookup({
+    "disable_defaults": False,
+    # any dictionary entry overiddes the defaults
+    "spelling": {"TP": "f"},  # dict: right handed only finger spelling
+    "symbols": {},  # dict: left hand symbols
+    "shifted": {},  # set: any symbols that should be shifted
+    "escape": "escape",  # default: "escape"
+    "right_hand": {},  # dict: easymotion finger spelling (right hand)
+    "left_hand": {},  # dict: easymotion finger spelling (left hand)
+    "systems": [
+        {
+            "unique_ender": "D",
+            # some ideas for others if you ever run out: -TZ, -SD, -TDZ, -SDZ, -TSZ, -TSD, or -TSDZ
+            "mods": {
+                # "-FPB": "",  #
+                "-7B": "",  # CHrome (vimium)
+                # "-FB": "space space f",  # Forward Back
+                # "-PB": "shift(t)",  # Previous Backwards (in command_letter)
+                # "-F": "f",  # Forwards (in command_letter)
+                # "-P": "t",  # Previous (in command_letter)
+                # "-B": "shift(f)",  # Backwards (in command_letter)
+                # "": ""  # for normal commands (in command_letter)
+                }
+            },
+        ]})
 
 
 assert easy_motion_lookup.dictionary.longest_key == LONGEST_KEY
@@ -73,7 +73,7 @@ assert easy_motion_lookup.dictionary.longest_key == LONGEST_KEY
 def lookup(key):
     for look in [
             easy_motion_lookup,
-            # chrome_lookup,
+            chrome_lookup,
             ]:
         with suppress(KeyError):
             return look(key)
