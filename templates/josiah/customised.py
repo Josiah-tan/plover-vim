3bc212bd5be73af76fb3f30325606167dafef065
Josiah-tan <josiah123t@gmail.com>
2022-08-10 14:39:59 +1000

    experimenting with a personal preference e.g.: 04 verses 440

--- templates/josiah/customised.py
+++ templates/josiah/customised.py
@@ -13,6 +13,8 @@ sys.path.append(repository_location)
 # from command_object.builtins import Lookup as CommandObjectLookup
 from command_letter_2.builtins import Lookup as CommandLetterLookup2
 from relative_number.builtins import Lookup as OldRelativeNumberLookup
+from relative_number.util import Zeroes, reverse, double
+from relative_number.defaults import up_down_system, clock_system, classic_system
 from relative_number.builtins import RelativeNumberLookup as RelativeNumberLookup
 from josiah_modifier.builtins import Lookup as JosiahModifierLookup
 
@@ -124,24 +126,90 @@ command_object_lookup_2 = CommandLetterLookup2({
 
 # relative_number_lookup = OldRelativeNumberLookup()
 relative_number_lookup = RelativeNumberLookup({
-        "disable_defaults": False,
-        "systems": {
-            "triple": {
+    "disable_defaults": True,
+    "numbers": {'S-': '1', 'T-': '2', 'P-': '3', 'H-': '4', 'A-': '5', 'O-': '6', '-F': '7', '-P': '8', '-L': '9'},
+    "systems": {
+        **up_down_system,
+        **clock_system,
+        ** classic_system,
+        # "zeros": {"dependencies": ["0" * i for i in range(1, 10)]},
+        "0reverse": {
+            "stroke": "U",
+            "callback": lambda x: x[::-1] if x[-1] == "0" else None,
+            "min_number": 10,
+            "max_number": 90,
+            "dependencies": ["0"]
+            },
+        "0": {
+            "stroke": "-S",
+            "callback": Zeroes(1),
+            "additional_map": "0",
+            "dependencies": ["reverseU"]
+            },
+        "0" * 2: {
+                "stroke": "-SZ",
+                "callback": Zeroes(2),
+                "additional_map": "0" * 2,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 3: {
+                "stroke": "-Z",
+                "callback": Zeroes(3),
+                "additional_map": "0" * 3,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 4: {
+                "stroke": "-TS",
+                "callback": Zeroes(4),
+                "additional_map": "0" * 4,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 5: {
+                "stroke": "-TSDZ",
+                "callback": Zeroes(5),
+                "additional_map": "0" * 5,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 6: {
+                "stroke": "-DZ",
+                "callback": Zeroes(6),
+                "additional_map": "0" * 6,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 7: {
+                "stroke": "-T",
+                "callback": Zeroes(7),
+                "additional_map": "0" * 7,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 8: {
+                "stroke": "-TD",
+                "callback": Zeroes(8),
+                "additional_map": "0" * 8,
+                "dependencies": ["reverseU", "doubleU"]
+                },
+        "0" * 9: {
+                "stroke": "-D",
+                "callback": Zeroes(9),
+                "additional_map": "0" * 9,
+                "dependencies": ["reverseU", "doubleU"],
+                },
+        "triple": {
                 "stroke": "E",
                 "callback": lambda x: x*3,
                 "min_number": 0,
                 "max_number": 9,
                 },
-            "slash": {
+        "slash": {
                 "stroke": "E",
                 "callback": lambda x: x[0] + "/" + x[1:],
                 "dependencies": ["reverseU"],
                 "min_number": 10,
                 "max_number": 99,
                 }
-            }
-    
+        }
     })
+dict(relative_number_lookup.dictionary.items())
 
 josiah_modifier_lookup = JosiahModifierLookup()
 
